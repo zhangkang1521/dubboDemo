d9e2ee38ba4af6a02a24b482ee731c0cdedc96db
zhangkang <zhangkang@lvmama.com>
2020-10-16 09:34:34 +0800

    cat测试

--- provider/src/main/java/org/zk/dubbo/provider/DemoServiceImpl.java
+++ provider/src/main/java/org/zk/dubbo/provider/DemoServiceImpl.java
@@ -1,5 +1,6 @@
 package org.zk.dubbo.provider;
 
+import com.alibaba.dubbo.rpc.RpcContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zk.dubbo.interfaces.DemoService;
@@ -13,6 +14,8 @@ public class DemoServiceImpl implements DemoService {
     private static Logger logger = LoggerFactory.getLogger(DemoServiceImpl.class);
 
     public String sayHello(String name) {
+        RpcContext rpcContext  = RpcContext.getContext();
+        System.out.println(rpcContext.getAttachment("application"));
         logger.info("=============== sayHello invoked ===========");
         return "hello, local," + name;
     }
