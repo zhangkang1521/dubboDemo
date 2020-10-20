2776ca004651dd96917c7a11f7f8187e418a2b47
zhangkang <zhangkang@lvmama.com>
2020-10-20 10:44:55 +0800

    圈子zookeeper节点

--- consumer/src/main/java/org/zk/dubbo/consumer/Consumer.java
+++ consumer/src/main/java/org/zk/dubbo/consumer/Consumer.java
@@ -8,13 +8,16 @@ import org.zk.dubbo.interfaces.DemoService;
  */
 public class Consumer {
     public static void main( String[] args ) throws Exception{
+        // 1个服务：ls /dubbo/CommonConfiguration/ROUTE_CYCLE_10.43.2.86=10.43.2.86@dubbo-b
+        // 2个服务：ls /dubbo/CommonConfiguration/ROUTE_CYCLE_10.43.2.86=10.43.2.86@dubbo-b,10.43.2.75@dubbo-b
+
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 new String[] { "applicationContext.xml" });
         context.start();
 
         DemoService demoService = (DemoService) context.getBean("demoService");
 //        System.out.println(JSON.toJSONString(demoService));
-        for (int i=0; i< 1; i++) {
+        for (int i=0; i< 10; i++) {
             System.out.println(demoService.sayHello("zhangkang"));
         }
         System.in.read();
